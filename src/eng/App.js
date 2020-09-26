ae8330f1a0fde0030a1ba2496e55f4dc7373c7ef
Ignacio <warkanlock@gmail.com>
2020-09-26 13:16:57 -0300

    Changes over folder structure

---
+++ src/eng/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./src/components/Shared/Navbar";
+import Dashboard from "./src/components/Root/Dashboard";
+
+const App = () => {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Dashboard></Dashboard>
+    </>
+  );
+};
+
+export default App;
